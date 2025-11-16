8f55aaf87680ec8eb2222558d351249671e6fd53
xFelipeA <Porps85@gmail.com>
2025-11-16 19:37:16 -0300

    Add User and Cliente models with SQLAlchemy

--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(200), unique=True)
+    password = db.Column(db.String(200))
+    role = db.Column(db.String(50))  # admin, gerente, funcionário
+
+class Cliente(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(200))
+    email = db.Column(db.String(200))
+    cnpj = db.Column(db.String(50))
+    comissao = db.Column(db.Float)
